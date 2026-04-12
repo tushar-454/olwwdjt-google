b3b500d5f00eff4dfa10936e380442fcbd9b4268
tushar-454 <imtushar454@gmail.com>
2026-04-13 00:52:49 +0600

    feat: add MobileMenu component for mobile navigation

---
+++ components/header/mobile-menu.tsx
@@ -0,0 +1,36 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { ListIcon } from "@phosphor-icons/react/dist/ssr";
+import Logo from "./logo";
+import NavItems from "./nav-items";
+import ScheduleMeetingBtn from "./schedule-meeting-btn";
+
+export default function MobileMenu() {
+    return (
+        <Sheet>
+            <SheetTrigger className="ml-auto lg:hidden">
+                <ListIcon
+                    size={40}
+                    className="rounded-lg bg-gray-100 p-2 dark:bg-gray-900"
+                />
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>
+                        <Logo />
+                    </SheetTitle>
+                    <SheetDescription className="mt-5">
+                        <NavItems isMobile />
+                        <ScheduleMeetingBtn isMobile />
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+    );
+}
