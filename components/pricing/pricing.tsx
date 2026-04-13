491e946238685948080c8a71bb2fd36e201bb290
tushar-454 <imtushar454@gmail.com>
2026-04-13 23:50:56 +0600

    feat: refactor Pricing component to manage isYearly state internally

--- components/pricing/pricing.tsx
+++ components/pricing/pricing.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { useState } from "react";
 import Container from "../common/container";
 import SectionHead from "../common/section-head";
 import PriceUnitSwitch from "./price-unit-switch";
 import PricingCards from "./pricing-cards";
 
-export default function Pricing({ isYearly }: { isYearly: boolean }) {
+export default function Pricing() {
+    const [isYearly, setIsYearly] = useState(false);
     return (
         <section className="py-10" id="pricing">
             <Container className="max-w-300">
@@ -15,7 +18,10 @@ export default function Pricing({ isYearly }: { isYearly: boolean }) {
                     descriptionClassName="text-lg sm:text-xl font-normal"
                 />
                 {/* pricing month - year switching */}
-                <PriceUnitSwitch />
+                <PriceUnitSwitch
+                    isYearly={isYearly}
+                    setIsYearly={setIsYearly}
+                />
                 {/* pricing table content */}
                 <PricingCards isYearly={isYearly} />
             </Container>
