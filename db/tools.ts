0a55a5fc2cdb5439f6135ec8c7a8fe56bf1a440d
tushar-454 <imtushar454@gmail.com>
2026-04-14 00:18:11 +0600

    feat: add tools data structure with type definition

---
+++ db/tools.ts
@@ -0,0 +1,53 @@
+export type TTool = {
+    id: number;
+    image: string;
+    name?: string;
+};
+
+export const tools: TTool[] = [
+    {
+        id: 1,
+        image: "/assets/pricelabs.png",
+        name: "Pricelabs",
+    },
+    {
+        id: 2,
+        image: "/assets/wheelhouse.png",
+        name: "Wheelhouse",
+    },
+    {
+        id: 3,
+        image: "/assets/beyondpricing.png",
+        name: "Beyond Pricing",
+    },
+    {
+        id: 4,
+        image: "/assets/hostfully.png",
+        name: "Hostfully",
+    },
+    {
+        id: 5,
+        image: "/assets/guestyforhosts.png",
+        name: "Guesty for Hosts",
+    },
+    {
+        id: 6,
+        image: "/assets/lodgify.png",
+        name: "Lodgify",
+    },
+    {
+        id: 7,
+        image: "/assets/hostfully.png",
+        name: "Hostfully",
+    },
+    {
+        id: 8,
+        image: "/assets/guestyforhosts.png",
+        name: "Guesty for Hosts",
+    },
+    {
+        id: 9,
+        image: "/assets/lodgify.png",
+        name: "Lodgify",
+    },
+];
