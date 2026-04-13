7e5c95cf1cd972c460dba3718bdb95f00777a140
tushar-454 <imtushar454@gmail.com>
2026-04-13 23:42:34 +0600

    feat: implement FeaturesLists component to display pricing features

---
+++ components/pricing/features-lists.tsx
@@ -0,0 +1,36 @@
+import { pricingFeatures, type TPricingType } from "@/db/pricing";
+import { cn } from "@/lib/utils";
+import CheckIcon from "./check-icon";
+import CrossIcon from "./cross-icon";
+
+type FeaturesListsProps = {
+    type: TPricingType;
+    isHighlighted?: boolean;
+};
+
+export default function FeaturesLists({
+    type,
+    isHighlighted = false,
+}: FeaturesListsProps) {
+    return (
+        <ul className="mt-6 space-y-3">
+            {pricingFeatures.map((feature) => (
+                <li
+                    key={feature.id}
+                    className={cn(
+                        "flex items-center gap-2",
+                        isHighlighted ? "text-neutral-50" : "text-neutral-500"
+                    )}
+                >
+                    {feature.availableIn.includes(type) ? (
+                        <CheckIcon />
+                    ) : (
+                        <CrossIcon isHighlighted={isHighlighted} />
+                    )}
+
+                    {feature.name}
+                </li>
+            ))}
+        </ul>
+    );
+}
