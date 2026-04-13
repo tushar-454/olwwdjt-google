ea83cf46e2d182fad586b8e4952b0d2658ab519d
tushar-454 <imtushar454@gmail.com>
2026-04-13 23:43:25 +0600

    feat: update Page component to include dynamic Pricing rendering based on subscription unit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import Hero from "@/components/hero/hero";
 import PartnerLogosStrip from "@/components/partner-logos-strip/partner-logos-strip";
+import Pricing from "@/components/pricing/pricing";
 import Services from "@/components/services/services";
 
-export default function Page() {
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+    const { unit } = await searchParams;
+
     return (
         <main>
             <Hero />
             <PartnerLogosStrip />
             <Services />
+            <Pricing isYearly={unit === "yearly"} />
         </main>
     );
 }
