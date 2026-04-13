531b1a4baffbb5ebf43d8fc477413ac276240ac9
tushar-454 <imtushar454@gmail.com>
2026-04-13 18:28:31 +0600

    feat: create Services component to display a grid of service cards with section header

---
+++ components/services/services.tsx
@@ -0,0 +1,25 @@
+import { services } from "@/db/services";
+import Container from "../common/container";
+import SectionHead from "../common/section-head";
+import ServiceCard from "./service-card";
+
+export default function Services() {
+    return (
+        <section>
+            <Container className="max-w-310">
+                <SectionHead
+                    title="Our"
+                    highlightedTitle="Services"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+
+                {/* services grid content */}
+                <div className="mt-13.5 grid grid-cols-1 gap-6.25 sm:grid-cols-2 lg:grid-cols-3">
+                    {services.map((service) => (
+                        <ServiceCard key={service.id} service={service} />
+                    ))}
+                </div>
+            </Container>
+        </section>
+    );
+}
