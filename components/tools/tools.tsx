7ce828425d27cd11735875609937cf884cd2db2e
tushar-454 <imtushar454@gmail.com>
2026-04-14 00:18:26 +0600

    feat: implement Tools component to display a grid of tools

---
+++ components/tools/tools.tsx
@@ -0,0 +1,25 @@
+import { tools } from "@/db/tools";
+import Container from "../common/container";
+import SectionHead from "../common/section-head";
+import ToolCard from "./tool-card";
+
+export default function Tools() {
+    return (
+        <section className="py-10" id="tools">
+            <SectionHead
+                title="Our"
+                highlightedTitle="Tools"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+
+            {/* services grid content */}
+            <div className="mt-24 bg-[#F6F6F6] py-9.5">
+                <Container className="grid max-w-5xl grid-cols-1 gap-6.25 min-[500px]:grid-cols-2 md:grid-cols-3">
+                    {tools.map((tool) => (
+                        <ToolCard key={tool.id} tool={tool} />
+                    ))}
+                </Container>
+            </div>
+        </section>
+    );
+}
