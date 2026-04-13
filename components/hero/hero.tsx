766f0678ab9013e3ef39e9fa9651c41d9147ece2
tushar-454 <imtushar454@gmail.com>
2026-04-13 16:33:15 +0600

    feat: update Hero component for improved styling and add showAlways prop to ScheduleMeetingBtn

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -5,26 +5,26 @@ import FloatingItems from "./floating-items";
 
 export default function Hero() {
     return (
-        <section className="flex h-145 items-end justify-center bg-[url('/assets/herobg.png')] bg-cover bg-center">
-            <Container className="relative max-w-230.75 rounded-[923px] bg-[rgba(255,255,255,0.62)] py-16 dark:bg-[rgba(255,255,255,0.05)]">
+        <section className="flex h-132 items-end justify-center bg-[url('/assets/herobg.png')] bg-cover bg-center sm:h-145">
+            <Container className="relative w-auto rounded-[923px] bg-[rgba(255,255,255,0.62)] py-16 lg:max-w-230.75 dark:bg-[rgba(255,255,255,0.05)]">
                 {/* floating items (logo, dot) */}
                 <FloatingItems />
                 <div className="flex flex-col items-center gap-8">
-                    <h1 className="flex flex-col items-center text-center text-[48px] leading-[130.187%] font-bold">
+                    <h1 className="flex flex-col items-center text-center text-2xl leading-[130.187%] font-bold min-[360px]:text-3xl sm:text-4xl md:text-[48px]">
                         Airbnb Assistants For{" "}
-                        <span className="text-[38.125px] font-medium">
+                        <span className="text-lg font-medium min-[360px]:text-2xl sm:text-3xl md:text-[38.125px]">
                             {" "}
                             Property Management
                         </span>
                     </h1>
-                    <p className="max-w-166.25 text-center text-sm leading-normal font-medium">
+                    <p className="w-auto text-center text-sm leading-normal font-medium md:max-w-166.25">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua. Ut enim ad minim veniam, quis nostrud
                         exercitation ullamco laboris nisi ut aliquip ex ea
                         commodo consequat.{" "}
                     </p>
-                    <ScheduleMeetingBtn />
+                    <ScheduleMeetingBtn showAlways />
                     <Link
                         href="#pricing"
                         className="text-center text-sm font-medium underline underline-offset-2"
