d3e20ac34becc88b9f1c025cfa571bdd31629275
tushar-454 <imtushar454@gmail.com>
2026-04-13 23:50:47 +0600

    feat: remove async keyword from PricingCards component

--- components/pricing/pricing-cards.tsx
+++ components/pricing/pricing-cards.tsx
@@ -1,11 +1,7 @@
 import { pricingPlans } from "@/db/pricing";
 import PricingCard from "./pricing-card";
 
-export default async function PricingCards({
-    isYearly,
-}: {
-    isYearly: boolean;
-}) {
+export default function PricingCards({ isYearly }: { isYearly: boolean }) {
     return (
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
             {pricingPlans.map((plan) => (
