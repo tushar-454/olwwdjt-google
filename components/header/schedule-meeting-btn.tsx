480f82a9c815534a241c6d1701659cd1e6997e45
tushar-454 <imtushar454@gmail.com>
2026-04-13 16:31:22 +0600

    feat: enhance ScheduleMeetingBtn to support showAlways prop for visibility control

--- components/header/schedule-meeting-btn.tsx
+++ components/header/schedule-meeting-btn.tsx
@@ -1,20 +1,24 @@
+import { cn } from "@/lib/utils";
 import { ArrowRightIcon } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
 type ScheduleMeetingBtnProps = {
     isMobile?: boolean;
+    showAlways?: boolean;
 };
 
 export default function ScheduleMeetingBtn({
     isMobile = false,
+    showAlways = false,
 }: ScheduleMeetingBtnProps) {
     return (
         <Link
             href={"#schedule-meeting"}
-            className={
-                isMobile ? "mt-5 inline-block lg:hidden" : "hidden lg:block"
-            }
+            className={cn(
+                isMobile ? "mt-5 inline-block lg:hidden" : "hidden lg:block",
+                showAlways && "inline-block"
+            )}
         >
             <Button className="flex h-12.5 items-center justify-center gap-2.5 rounded-[5px] px-4.5 py-6">
                 <span className="text-center text-[14px] leading-[100%] font-bold text-white">
