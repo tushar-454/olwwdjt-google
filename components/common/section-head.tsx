e16c3cec57f5a42b61a2acb54821cd12a68dc875
tushar-454 <imtushar454@gmail.com>
2026-04-13 23:40:52 +0600

    feat: enhance SectionHead component to accept custom class names for title and description

--- components/common/section-head.tsx
+++ components/common/section-head.tsx
@@ -1,20 +1,36 @@
+import { cn } from "@/lib/utils";
+
 type SectionHeadProps = {
     title: string;
     highlightedTitle: string;
     description: string;
+    titleClassName?: string;
+    descriptionClassName?: string;
 };
 
 export default function SectionHead({
     title,
     highlightedTitle,
     description,
+    titleClassName,
+    descriptionClassName,
 }: SectionHeadProps) {
     return (
         <div className="flex flex-col items-center gap-3.5">
-            <h1 className="text-center text-[32px] leading-normal font-bold">
+            <h1
+                className={cn(
+                    "text-center text-[32px] leading-normal font-bold",
+                    titleClassName
+                )}
+            >
                 {title} <span className="text-primary">{highlightedTitle}</span>
             </h1>
-            <p className="w-auto text-center text-sm leading-normal font-medium md:max-w-184.5">
+            <p
+                className={cn(
+                    "w-auto text-center text-sm leading-normal font-medium md:max-w-184.5",
+                    descriptionClassName
+                )}
+            >
                 {description}
             </p>
         </div>
