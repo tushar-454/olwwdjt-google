73ec9729015e46c876183effffdf1c4c971ba3fe
tushar-454 <imtushar454@gmail.com>
2026-04-14 12:13:12 +0600

    feat: add additional font imports for enhanced typography options

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,20 @@ import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
-import { DM_Sans, Manrope } from "next/font/google";
+import { DM_Sans, Manrope, Plus_Jakarta_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"], variable: "--font-sans" });
 const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
+const poppins = Poppins({
+    subsets: ["latin"],
+    variable: "--font-poppins",
+    weight: ["400", "500", "600", "700"],
+});
+const plusja = Plus_Jakarta_Sans({
+    subsets: ["latin"],
+    variable: "--font-plusja",
+});
 
 export const metadata = {
     title: "Google",
@@ -27,7 +36,9 @@ export default function RootLayout({
                 "antialiased",
                 "font-sans",
                 dmSans.variable,
-                manrope.variable
+                manrope.variable,
+                poppins.variable,
+                plusja.variable
             )}
         >
             <body>
