76432aaa1ff1b9395d5944961ff2a50ec29ef515
tushar-454 <imtushar454@gmail.com>
2026-04-14 11:07:49 +0600

    feat: implement FAQs component to display frequently asked questions

---
+++ components/faqs/faqs.tsx
@@ -0,0 +1,21 @@
+import Container from "../common/container";
+import SectionHead from "../common/section-head";
+import FAQsAccordions from "./faqs-accordions";
+
+export default function FAQs() {
+    return (
+        <section className="mt-20 py-10" id="faqs">
+            <Container className="max-w-3xl">
+                <SectionHead
+                    title="Frequently asked"
+                    highlightedTitle="questions"
+                    description="Everything you need to know about the product and billing."
+                    descriptionClassName="text-lg sm:text-xl font-normal"
+                />
+
+                {/* faqs accordions  */}
+                <FAQsAccordions />
+            </Container>
+        </section>
+    );
+}
