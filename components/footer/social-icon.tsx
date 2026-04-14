e94d156c9d47dfb1cc0a77bde1eb726b606aff69
tushar-454 <imtushar454@gmail.com>
2026-04-14 12:07:47 +0600

    feat: add SocialIcon component for displaying social media links in the footer

---
+++ components/footer/social-icon.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const socialIcons = [
+    {
+        src: "/assets/twitter.png",
+        alt: "twitter",
+        href: "#",
+    },
+    {
+        src: "/assets/facebook.png",
+        alt: "facebook",
+        href: "#",
+    },
+    {
+        src: "/assets/instagram.png",
+        alt: "instagram",
+        href: "#",
+    },
+    {
+        src: "/assets/github.png",
+        alt: "github",
+        href: "#",
+    },
+];
+
+export default function SocialIcon() {
+    return (
+        <div className="flex items-center gap-8">
+            {socialIcons.map((icon) => (
+                <Link
+                    key={icon.src}
+                    href={icon.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={24}
+                        height={24}
+                        className="h-5 w-5 cursor-pointer object-contain"
+                    />
+                </Link>
+            ))}
+        </div>
+    );
+}
