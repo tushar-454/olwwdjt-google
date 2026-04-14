1b50ff58575962de799bcfaf776ade52828ba7f3
tushar-454 <imtushar454@gmail.com>
2026-04-14 10:33:09 +0600

    feat: add ReviewsCards component for displaying a list of user reviews

---
+++ components/client-reviews/reviews-cards.tsx
@@ -0,0 +1,15 @@
+import { reviews } from "@/db/reviews";
+import Container from "../common/container";
+import ReviewsCard from "./reviews-card";
+
+export default function ReviewsCards() {
+    return (
+        <div className="mt-13.5">
+            <Container className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {reviews.map((review) => (
+                    <ReviewsCard key={review.id} review={review} />
+                ))}
+            </Container>
+        </div>
+    );
+}
