c4bd407d4995d957c75422718d84122a1f348231
tushar-454 <imtushar454@gmail.com>
2026-04-14 12:08:07 +0600

    feat: implement FooterColumns component for structured footer layout

---
+++ components/footer/footer-columns.tsx
@@ -0,0 +1,12 @@
+import { footerColumns } from "@/db/footer";
+import FooterColumn from "./footer-column";
+
+export default function FooterColumns() {
+    return (
+        <div className="col-span-3 grid grid-cols-1 gap-10 md:grid-cols-3">
+            {footerColumns.map((column) => (
+                <FooterColumn key={column} column={column} />
+            ))}
+        </div>
+    );
+}
