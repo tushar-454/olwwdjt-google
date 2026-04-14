45a36f0ff50a7443604c89f03b2284b8ed7564de
tushar-454 <imtushar454@gmail.com>
2026-04-14 12:17:34 +0600

    feat: improve footer responsiveness with min-width grid adjustment

--- components/footer/footer-columns.tsx
+++ components/footer/footer-columns.tsx
@@ -3,7 +3,7 @@ import FooterColumn from "./footer-column";
 
 export default function FooterColumns() {
     return (
-        <div className="col-span-3 grid grid-cols-1 gap-10 md:grid-cols-3">
+        <div className="col-span-3 grid grid-cols-1 gap-10 min-[500px]:grid-cols-2 md:grid-cols-3">
             {footerColumns.map((column) => (
                 <FooterColumn key={column} column={column} />
             ))}
