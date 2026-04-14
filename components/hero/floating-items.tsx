1da06f5e0d7fe1686f3c359165e246f56528719b
tushar-454 <imtushar454@gmail.com>
2026-04-14 22:35:59 +0600

    feat: add pulse animations to dots for enhanced visual effects

--- components/hero/floating-items.tsx
+++ components/hero/floating-items.tsx
@@ -37,17 +37,17 @@ export default function FloatingItems() {
             />
 
             {/* lefts dots  */}
-            <span className="absolute top-10 -left-32 inline-block h-3.25 w-3.25 rounded-full bg-[#635BFF] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#635BFF]/70"></span>
-            <span className="absolute top-1/2 -left-10 inline-block h-3.25 w-3.25 rounded-full bg-[#FF5A5F] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FF5A5F]/70"></span>
-            <span className="absolute bottom-24 -left-32 inline-block h-1.75 w-1.75 rounded-full bg-[#FFEC5A] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FFEC5A]/70"></span>
+            <span className="absolute top-10 -left-32 inline-block h-3.25 w-3.25 animate-pulse rounded-full bg-[#635BFF] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#635BFF]/70"></span>
+            <span className="absolute top-1/2 -left-10 inline-block h-3.25 w-3.25 animate-pulse rounded-full bg-[#FF5A5F] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FF5A5F]/70"></span>
+            <span className="absolute bottom-24 -left-32 inline-block h-1.75 w-1.75 animate-pulse rounded-full bg-[#FFEC5A] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FFEC5A]/70"></span>
 
             {/* center dots  */}
-            <span className="absolute top-0 left-1/2 inline-block h-3.75 w-3.75 rounded-full bg-[#34E0A1] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#34E0A1]/70"></span>
-            <span className="absolute bottom-4 left-1/2 inline-block h-3.25 w-3.25 rounded-full bg-[#5AF9FF] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#5AF9FF]/70"></span>
+            <span className="absolute top-0 left-1/2 inline-block h-3.75 w-3.75 animate-pulse rounded-full bg-[#34E0A1] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#34E0A1]/70"></span>
+            <span className="absolute bottom-4 left-1/2 inline-block h-3.25 w-3.25 animate-pulse rounded-full bg-[#5AF9FF] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#5AF9FF]/70"></span>
 
             {/* right dots  */}
-            <span className="absolute top-32 -right-20 inline-block h-3.25 w-3.25 rounded-full bg-[#FFEC5A] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FFEC5A]/70"></span>
-            <span className="absolute -right-40 bottom-32 inline-block h-1.75 w-1.75 rounded-full bg-[#0C3B7C] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#0C3B7C]/70"></span>
+            <span className="absolute top-32 -right-20 inline-block h-3.25 w-3.25 animate-pulse rounded-full bg-[#FFEC5A] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#FFEC5A]/70"></span>
+            <span className="absolute -right-40 bottom-32 inline-block h-1.75 w-1.75 animate-pulse rounded-full bg-[#0C3B7C] shadow-[inset_0_-2px_6.3px_0_rgba(0,0,0,0.25)] dark:bg-[#0C3B7C]/70"></span>
         </div>
     );
 }
